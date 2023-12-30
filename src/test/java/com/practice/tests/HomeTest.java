3a0bb47b27bfd9f7296ebcc5effec5d933504955
mahesh.pund <devops.maheshpund@gmail.com>
2023-12-30 14:23:37 +0530

    New Project

---
+++ src/test/java/com/practice/tests/HomeTest.java
@@ -0,0 +1,13 @@
+package com.practice.tests;
+
+import org.testng.annotations.Test;
+
+import com.practice.utils.BaseUtils;
+
+public class HomeTest extends BaseUtils {
+
+	@Test
+	public void test() {
+		System.out.println("Start");
+	}
+}
